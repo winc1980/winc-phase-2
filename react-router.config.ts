779112347f9bf6f91ed6865c24edb57b31da9f1f
hattori-expertbook <hattori.index.js@gmail.com>
2026-08-12 20:12:53 +0900

    fix: その他もフォーマット

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,7 +1,7 @@
-import type { Config } from "@react-router/dev/config";
+import type { Config } from "@react-router/dev/config"
 
 export default {
-  // サーバーサイドレンダリングはデフォルトで有効
-  // SPAを構築する際にはfalseに設定
-  ssr: true,
-} satisfies Config;
+	// サーバーサイドレンダリングはデフォルトで有効
+	// SPAを構築する際にはfalseに設定
+	ssr: true,
+} satisfies Config
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
-import { reactRouter } from "@react-router/dev/vite";
-import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "vite";
+import { reactRouter } from "@react-router/dev/vite"
+import tailwindcss from "@tailwindcss/vite"
+import { defineConfig } from "vite"
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter()],
-  resolve: {
-    tsconfigPaths: true,
-  },
-});
+	plugins: [tailwindcss(), reactRouter()],
+	resolve: {
+		tsconfigPaths: true,
+	},
+})
