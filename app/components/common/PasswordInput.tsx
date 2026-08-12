ea55617b1d9be7423b729f1b755926d46023006e
hattori-expertbook <hattori.index.js@gmail.com>
2026-08-12 15:35:28 +0900

    feat: ログイン画面を作成

---
+++ app/components/common/PasswordInput.tsx
@@ -0,0 +1,27 @@
+import { EyeIcon, EyeOffIcon } from "lucide-react"
+import { useState } from "react"
+import { Button } from "../ui/button"
+import { Input } from "../ui/input"
+
+export function PasswordInput({ id }: { id: string }) {
+	const [show, setShow] = useState(false)
+
+	return (
+		<div className="relative">
+			<Input id={id} type={show ? "text" : "password"} />
+			<Button
+				onClick={() => setShow((prev) => !prev)}
+				className="absolute top-0 right-0 h-full px-3 hover:bg-transparent"
+				size="icon"
+				type="button"
+				variant="ghost"
+			>
+				{show ? (
+					<EyeOffIcon className="size-4 text-muted-foreground" />
+				) : (
+					<EyeIcon className="size-4 text-muted-foreground" />
+				)}
+			</Button>
+		</div>
+	)
+}
--- app/routes/auth/login.tsx
+++ app/routes/auth/login.tsx
@@ -1,3 +1,36 @@
+import { PasswordInput } from "~/components/common/PasswordInput"
+import { Card, CardContent, CardHeader, CardTitle } from "~/components/ui/card"
+import {
+	Field,
+	FieldDescription,
+	FieldGroup,
+	FieldLabel,
+	FieldSet,
+} from "~/components/ui/field"
+import { Input } from "~/components/ui/input"
+
 export default function LoginPage() {
-	return <div>Login</div>
+	return (
+		<div className="h-full flex flex-col justify-center items-center">
+			<Card className="w-full max-w-xl">
+				<CardHeader>
+					<CardTitle>ログイン</CardTitle>
+				</CardHeader>
+				<CardContent>
+					<FieldSet>
+						<FieldGroup>
+							<Field>
+								<FieldLabel htmlFor="mail">早稲田メールアドレス</FieldLabel>
+								<Input id="mail" placeholder="waseda@asagi.waseda.jp" />
+							</Field>
+							<Field>
+								<FieldLabel htmlFor="password">パスワード</FieldLabel>
+								<PasswordInput id="password" />
+							</Field>
+						</FieldGroup>
+					</FieldSet>
+				</CardContent>
+			</Card>
+		</div>
+	)
 }
